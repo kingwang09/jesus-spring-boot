a9dc40e33e90be3ff95d4038270d82ed4ae397bc
hyungeun.jin <kingwang09@gmail.com>
2023-04-11 16:53:51 +0900

    spring-web이 있으면 reactive가 안돌아가게됨. 예제는 주석처리하도록 한다.

--- src/main/java/org/jesus/spring/greet/webflux/GreetingRouter.java
+++ src/main/java/org/jesus/spring/greet/webflux/GreetingRouter.java
@@ -3,19 +3,19 @@ package org.jesus.spring.greet.webflux;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.server.RouterFunction;
-import org.springframework.web.reactive.function.server.RouterFunctions;
-import org.springframework.web.reactive.function.server.ServerResponse;
+//import org.springframework.web.reactive.function.server.RouterFunction;
+//import org.springframework.web.reactive.function.server.RouterFunctions;
+//import org.springframework.web.reactive.function.server.ServerResponse;
+//
+//import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+//import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
-
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class GreetingRouter {
 
-    @Bean
-    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
-        return RouterFunctions
-                .route(GET("/reactive/hello").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::hello);
-    }
+//    @Bean
+//    public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
+//        return RouterFunctions
+//                .route(GET("/reactive/hello").and(accept(MediaType.APPLICATION_JSON)), greetingHandler::hello);
+//    }
 }
