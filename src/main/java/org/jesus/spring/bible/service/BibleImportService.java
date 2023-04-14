f17e644e1d385772fb2b8a70c6b5c4760820bf6f
hyungeun.jin <kingwang09@gmail.com>
2023-04-14 11:02:52 +0900

    성경 인덱스 parsing 로직 개선: 하나로 통합

--- src/main/java/org/jesus/spring/bible/service/BibleImportService.java
+++ src/main/java/org/jesus/spring/bible/service/BibleImportService.java
@@ -17,24 +17,37 @@ import java.util.stream.Stream;
 public class BibleImportService {
 
     private final String PATTERN_BIBLE = "([ㄱ-ㅎ가-힣])+|([\\d])+";
+    public static final String BIBLE_INDEX_KEY = "bibleIndex";
+    public static final String BIBLE_CHAPTER_KEY = "bibleChapter";
+    public static final String BIBLE_LINE_KEY = "bibleLine";
+    public static final String BIBLE_INDEX_VALUE = "bibleValue";
 
-    public String parsingBibleIndex(String value){
-        int index = value.indexOf(" ");
-        String indexValue = value.substring(0, index);
-        log.debug("index: {}", indexValue);
-        return indexValue;
+    public static final int BIBLE_INDEX = 0;
+    public static final int BIBLE_CHAPTER = 1;
+    public static final int BIBLE_LINE = 2;
+
+    public Map<String, String> parsingLines(String lines){
+        Map<String, String> result = new HashMap<>();
+        int index = lines.indexOf(" ");
+        String bibleIndex = lines.substring(0, index);
+        String bibleValue = lines.substring(index + 1);
+
+        Map<Integer, String> bibleIndexMap = parsingBibleIndexMap(bibleIndex);
+        result.put(BIBLE_INDEX_KEY, bibleIndexMap.get(BIBLE_INDEX));
+        result.put(BIBLE_CHAPTER_KEY, bibleIndexMap.get(BIBLE_CHAPTER));
+        result.put(BIBLE_LINE_KEY, bibleIndexMap.get(BIBLE_LINE));
+        result.put(BIBLE_INDEX_VALUE, bibleValue);
+        return result;
     }
 
-    public Map<Integer, String> parsingBibleIndexMap(String bibleIndexValue){
+    private Map<Integer, String> parsingBibleIndexMap(String bibleIndexValue){
         Map<Integer, String> result = new HashMap<>();
         int index = 0;
         Pattern p = Pattern.compile(PATTERN_BIBLE);	// 검색할 문자열 패턴
         Matcher m = p.matcher(bibleIndexValue);			// 문자열 설정
 
         while (m.find()) {
-            String value = m.group();
-            log.debug("result: {}", value);
-            result.put(index, value);
+            result.put(index, m.group());
             index++;
         }
         return result;
