e36611d9ea1f71ecb69fe30d12cf26c389230f06
hyungeun.jin <kingwang09@gmail.com>
2023-04-14 15:07:31 +0900

    file read 테스트 추가

--- src/test/java/org/jesus/spring/bible/service/FileImportServiceTest.java
+++ src/test/java/org/jesus/spring/bible/service/FileImportServiceTest.java
@@ -3,11 +3,28 @@ package org.jesus.spring.bible.service;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
 @Slf4j
 public class FileImportServiceTest {
 
     @Test
-    public void fileRead(){
-
+    public void fileRead() throws IOException {
+        //import할 파일 encoding이 UTF-8로 설정되어있어야함.
+        Path path = Paths.get("/Users/test/file_import/translation.txt");
+        List<String> lines = Files.readAllLines(path);
+        int index = 0;
+        for(String line : lines){
+            if(index == 10){
+                break;
+            }
+            log.debug("line: {}", line);
+            index++;
+        }
+        log.debug("test end...");
     }
 }
