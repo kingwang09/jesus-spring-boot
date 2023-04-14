9253ac9adcac4cc3f025fdd7f2c1d30c8f61e9e3
hyungeun.jin <kingwang09@gmail.com>
2023-04-14 10:39:38 +0900

    성경 인덱스 parsing 로직 개선: 인덱스 추출 로직

--- src/main/java/org/jesus/spring/bible/service/BibleImportService.java
+++ src/main/java/org/jesus/spring/bible/service/BibleImportService.java
@@ -18,10 +18,17 @@ public class BibleImportService {
 
     private final String PATTERN_BIBLE = "([ㄱ-ㅎ가-힣])+|([\\d])+";
 
+    public String parsingBibleIndex(String value){
+        int index = value.indexOf(" ");
+        String indexValue = value.substring(0, index);
+        log.debug("index: {}", indexValue);
+        return indexValue;
+    }
+
     public Map<Integer, String> parsingBibleIndexMap(String bibleIndexValue){
         Map<Integer, String> result = new HashMap<>();
         int index = 0;
-        Pattern p = Pattern.compile("([ㄱ-ㅎ가-힣])+|([\\d])+");	// 검색할 문자열 패턴
+        Pattern p = Pattern.compile(PATTERN_BIBLE);	// 검색할 문자열 패턴
         Matcher m = p.matcher(bibleIndexValue);			// 문자열 설정
 
         while (m.find()) {
